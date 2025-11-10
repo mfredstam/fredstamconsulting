381d47e40e7e99e341f11362cf545afb82707d6c
Marcus Fredstam <marcus.fredstam@gmail.com>
2025-11-10 20:20:08 +0100

    Inital commit, website looking pretty good

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Mobile Navbar Toggle
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	navLinks.classList.toggle("show");
+    });
+});
